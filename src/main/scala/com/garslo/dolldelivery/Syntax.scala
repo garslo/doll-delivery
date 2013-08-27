5f289ebd1ff002233a49cf71e6b23afccf0b9686
Gary Slopsema <gslopsema@gmail.com>
2013-08-27 15:57:49 -0400

    Comments

--- src/main/scala/com/garslo/dolldelivery/Syntax.scala
+++ src/main/scala/com/garslo/dolldelivery/Syntax.scala
@@ -3,8 +3,8 @@ package com.garslo.dolldelivery
 object MapSyntax extends DataSyntax {
   // Map edge lines look like
   // Map("startLocation" -> "<alphanumeric>", "endLocation" -> "<alphanumeric", "destination" -> <digits>)
+  // All the cruft is just allowing for whitespace
   val edgeRegex = """Map\("startLocation"\s*->\s*"(.*)"\s*,\s*"endLocation"\s*->\s*"(.*)"\s*,\s*"distance"\s*->\s*(\d+)\),?"""
-
   val startVertexRegex = """startingLocation:\s*"(.+)""""
   val endVertexRegex = """targetLocation:\s*"(.+)""""
 }
