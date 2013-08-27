46b92efa77ab3bd11a41c0257b8a282147731b17
Gary Slopsema <gslopsema@gmail.com>
2013-08-27 15:59:36 -0400

    Added application front-end

--- src/main/scala/com/garslo/dolldelivery/DeliverySolver.scala
+++ src/main/scala/com/garslo/dolldelivery/DeliverySolver.scala
@@ -1,11 +1,32 @@
 package com.garslo.dolldelivery
 
-class DeliverySolver(dataFile: java.lang.String) {
-  type Vertex = String
-  type Edge = Map[String, Any]
-  type Graph = List[Edge]
+object DeliverySolver {
+  def main(args: Array[String]) = {
+    args foreach { arg =>
+      try {
+        println(shortestPath(arg))
+      } catch {
+        case badSyntax: BadSyntaxException => println("Could not parse one of your files")
+        case e: Exception => println("We had a problem.")
+      }
+    }
+  }
 
-  def solve(): List[String] = {
-    List()
+  def shortestPath(file: String): Map[String, String]= {
+    // We default to the Map-type syntax, but csv works as well
+    var syntax: DataSyntax = MapSyntax
+    val extension = file.split("""\.""").last
+    if (extension == "csv") {
+      syntax = CsvSyntax
+    }
+
+    val (distance, shortestPath) = dijkstra(file, syntax)
+    Map("distance" -> distance.toString, "path" -> shortestPath.mkString(" => "))
+  }
+
+  def dijkstra(file: String, syntax: DataSyntax) = {
+    val (start, end, edges) = loadFile(file, syntax)
+    val d = new DijkstraAlgorithm(edges)
+    d.findShortestPath(start, end)
   }
 }
--- src/test/scala/com/garslo/dolldelivery/DeliverySolverSpec.scala
+++ src/test/scala/com/garslo/dolldelivery/DeliverySolverSpec.scala
@@ -4,10 +4,28 @@ import org.specs2._
 
 object DeliverySolverSpec extends mutable.Specification {
   "DeliverySolver" should {
-    "fail on bad input file" in {
-      val solver = new DeliverySolver("this-file-does-not-exist.txt")
-      val result = solver.solve()
-      result.length must_==(0)
+    val dataFiles = Seq(
+      "src/test/scala/com/garslo/dolldelivery/resources/test_data_1.txt",
+      "src/test/scala/com/garslo/dolldelivery/resources/test_data_2.csv",
+      "src/test/scala/com/garslo/dolldelivery/resources/test_data_3.csv"
+    )
+
+    "solve test data 1" in {
+      val (distance, path) = DeliverySolver.dijkstra(dataFiles(0), MapSyntax)
+      distance must be equalTo(31)
+      path.mkString(" => ") must be equalTo("Kruthika's abode => Brian's apartment => Wesley's condo => Bryce's den => Craig's haunt")
+    }
+
+    "solve test data 2" in {
+      val (distance, path) = DeliverySolver.dijkstra(dataFiles(1), CsvSyntax)
+      distance must be equalTo(3)
+      path.mkString(" => ") must be equalTo("A => C => F => E")
+    }
+
+    "solve test data 3" in {
+      val (distance, path) = DeliverySolver.dijkstra(dataFiles(2), MapSyntax)
+      distance must be equalTo(2)
+      path.mkString(" => ") must be equalTo("G => D => C")
     }
   }
 }
