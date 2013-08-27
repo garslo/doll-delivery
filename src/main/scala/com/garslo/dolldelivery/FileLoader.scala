759ce75071856c5350f298bd727205dd2df3f274
Gary Slopsema <gslopsema@gmail.com>
2013-08-27 18:42:38 -0400

    Closed file properly

--- src/main/scala/com/garslo/dolldelivery/FileLoader.scala
+++ src/main/scala/com/garslo/dolldelivery/FileLoader.scala
@@ -4,8 +4,9 @@ import scala.io.Source
 
 class FileLoader(extractor: DataExtractor) extends DataLoader {
   def load(filename: String) = {
-    val file = Source.fromFile(filename).mkString
-    val lines = file.split("\\r*\\n") map { _.trim }
+    val file = Source.fromFile(filename)
+    val lines = file.mkString.split("\\r*\\n") map { _.trim }
+    file.close()
 
     val edges = extractor.getEdges(lines)
     val start = extractor.getVertex(lines, "start")
